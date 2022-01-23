3ce635cfa7c56ceb6f9c30850218b1d859462f58
Bijay Chaudhary <sagtorabijay@gmail.com>
2022-01-23 23:15:22 +0545

    Revised the function with some corrections

--- class operators.java
+++
@@ -1,7 +0,0 @@
-class operators{
-    public static void main(String []args){
-        int obt=450, int tot=500;
-        double per=obt / tot * 100;
-        System.out.printf("The obtained percentage is: %f",per);
-    }
-}
--- operatorsType_Casting.java
+++ operatorsType_Casting.java
@@ -4,7 +4,7 @@ class operatorsType_Casting{
         double per= (double)obt / tot * 100;
         /*(double)obt is done so that Type casting will be feasible because when we devide int by int will resultant int and
          int producted with int will result the int as the result the Resulting answer might arrive in 0 as type<<int>> thus,
-         to avoid that we just typecasted the "obt" variable into double as a reult: {(double/int->int) && (int)} -> double;
+         to avoid that we just typecasted the "obt" variable into double as a result: {(double/int->int) && (int)} -> double;
         */ 
         System.out.printf("The obtained percentage is: %f \n",per);
     }
